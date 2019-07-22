3d067c217fe019abfffa200e6e1706baf4a86a31
bubbavox <adrianhwhite@gmail.com>
2019-07-22 14:29:28 -0400

    Update outline.rb

--- outline.rb
+++ outline.rb
@@ -50,13 +50,73 @@ side
 
 
 # pseudocode:
-:render board -- prompt player_x
-gets "input" [ l, r, ul, ur, dl, dr ]
-convert "input" to appropriate vector based on piece type/color
-sends direction "dir" to :move # e.g. dir = ul =  
-  check state of next_sqare 1*vector => valid? vacant?
-    if :vacant? => :move to Dest => next :turn
-    if occupied => checks vacancy of next_sq
-      if next_sq.vacant? => :jump => 2*vector
-        => player_x moves :again
-      if occupied => :move_invalid
+# :render board -- prompt player_x
+# gets "input" [ l, r, ul, ur, dl, dr ]
+# convert "input" to appropriate vector based on piece type/color
+# sends direction "dir" to :move # e.g. dir = ul =  
+#   check state of next_sqare 1*vector => valid? vacant?
+#     if :vacant? => :move to Dest => next :turn
+#     if occupied => checks vacancy of next_sq
+#       if next_sq.vacant? => :jump => 2*vector
+#         => player_x moves :again
+#       if occupied => :move_invalid
+
+
+# hypothetical :turn "master method"
+
+# class ?
+def turn(side, active_piece=nil)
+  # active_piece argument will be defined for double-jumps
+  if active_piece==nil     
+    print "Choose a piece to move"
+    active_piece = gets.chomp
+  end
+  print "What's your vector victor"
+  input = gets.chomp
+  vector = active_piece.convert_to_vector(input)
+  dest = active_piece.nav(vector)     # :nav turns vector into a move -- checks validity, jumps, crowning etc
+  active_piece.move(dest, crown?)      # or board.move ?
+  if again?
+    turn(side, active_piece)
+  else
+    turn(side*-1)
+end
+
+# class Piece
+def convert_to_vector(input)
+  if active_piece.is_king?
+    case input      # or simply 'vector = input' when king?
+    when "ul" @piece.ul    # how to call methods on Piece instances?
+    when "ur" @piece.ur
+    when "dl" @piece.dl
+    when "dr" @piece.dr
+    else puts input_error
+    end
+  else
+    case input
+    when "l"
+      piece.side==-1 ? @piece.ul : @piece.dl
+    when "r"
+      piece.side==-1 ? @piece.ur : @piece.dr
+    else puts input_error
+  end
+end
+
+# class Piece 
+def nav(vector)
+  # check vector*1
+    # is on board?
+    # if vacant, move
+  # else check vector*2 (jump) --> jump if vacant
+    # remove jumped piece from board                
+    # add jumped piece to captured array?
+    # move again or set jump? true
+  # check for :crown --> make king?
+end
+
+# class Piece
+def move(dest, crown?)
+  # updates dest in board array
+  # updates piece with new coordinates
+  # updates jumped pieces?
+end

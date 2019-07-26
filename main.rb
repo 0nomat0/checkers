941076ada0133e9ccb44cabc3f008f3c98cee062
phizeroth <phi.zeroth@gmail.com>
2019-07-25 20:24:47 -0400

    Objectify the board, pieces, and empty spaces.
    Board array now contains piece instance objects and empty space objects.
    Piece's display string is stored in @display.
    Empty spaces are now objects, with display string stored in display.
    Board#render now iterates through board.array, and simply prints each
    object's @display value.

--- main.rb
+++ main.rb
@@ -24,23 +24,24 @@ $sides = { -1 => "pink", 1 => "blue" }
   # Better to always see position in [x,y] pair
 
 class Piece
-  attr_accessor :id, :color, :side, :x, :y
+  attr_accessor :id, :side, :color, :x, :y, :display
 
-  def initialize (id, side, x, y, board) 
+  def initialize(id, side, x, y, board) 
     @id = id                # == single lowercase letter
     @side = side            # == -1 or 1
     @color = $sides[side]   # == color string from $sides global
     @x, @y = x, y           # == 0..7, 0..7 ; both should never be even or odd
+    @display = pieceify     # == display string from colorize module
     @board = board          # == board object
-    @board[@x, @y] = pieceify
+    @board[@x, @y] = self   # inserts itself into the board array
   end
   
   def l; move(@x-1, @y+@side) end
   def r; move(@x+1, @y+@side) end
   
   ## move ul,ur,dl,dr to King class?
-  # def ul; move(@x-1, @y-1) end
-  # def ur; move(@x+1, @y-1) end
+  def ul; move(@x-1, @y-1) end
+  def ur; move(@x+1, @y-1) end
   def dl; move(@x-1, @y+1) end
   def dr; move(@x+1, @y+1) end
 
@@ -49,8 +50,8 @@ class Piece
 
   # takes dest args, sets origin blank, assigns colored string to array dest, assigns dest to piece's x,y
   def move(new_x, new_y)
-    @board[@x, @y] = "   "
-    @board[new_x, new_y] = pieceify
+    @board[@x, @y] = EmptySquare.new
+    @board[new_x, new_y] = self
     @x, @y = new_x, new_y
   end
 
@@ -67,31 +68,37 @@ class King < Piece
     @id = piece.id.upcase       # == upcase original piece id
     @color = piece.color        # == color from piece
     @x, @y = piece.x, piece.y   # == pos from piece
+    @display = pieceify         # == display string from colorize module
     @board = board              # == board object
-    @board[@x, @y] = pieceify
+    @board[@x, @y] = self       # inserts itself into the board array
   end
 
 end # class King
 
 
+class EmptySquare
+  attr_accessor :display
+
+  def initialize(type=nil)
+    @display = type == :shade ? "░░░" : "   "
+  end
+
+end # class EmptySquare
+
+
 # @board contains 8 row arrays
 # each row array contains 4 positions + 4 blanks
 
-# TODO: Change board array to contain actual piece objects
-  # No need to create 24 variables for pieces, pieces can be located by Array.find(id)
-  # Is more object-y; when moved, the entire piece object is moved in the array
-  #   instead of just moving strings around (e.g. " c ", "ᵕMᵕ"). Board array contains
-  #   the actual object, render method prints the object's representation to the screen.
-
 class Board
-  attr_accessor :board
+  attr_accessor :grid
+
   def initialize
-    @board = Array.new(8) { Array.new(8) }
+    @array = Array.new(8) { Array.new(8) }   # creates empty array (filled with nils)
   
-    @board.each_with_index do |col, index|
-      n = index%2 == 0 ? 0 : 1
-      col.map!.with_index do |sq, index|
-        index%2 == n ? "░░░" : "   "
+    @array.each_with_index do |row, row_i|
+      row.map!.with_index do |square, col_i|
+        # if row and col are both even or odd, then insert shade, else empty
+        row_i%2 == col_i%2 ? EmptySquare.new(:shade) : EmptySquare.new
       end
     end
   end # class Board
@@ -101,19 +108,21 @@ class Board
     mid = "   ╠" + "═══╬"*7 + "═══╣"
     bot = "   ╚" + "═══╩"*7 + "═══╝"
     side = "║"
+
     print " "*4, "x", Array[*0..7].join("  x")
     print "\n", top, "\n"
+
     # loop for each of 8 row arrays
-    @board[0..7].each_with_index do |row, index|
+    @array[0..7].each_with_index do |row, index|
       print "y#{index} " + side
-      row.each { |state| print state + side }
+      row.each { |square| print square.display + side }
       print "\n", index == 7 ? bot : mid, "\n"
     end
-  end
+  end # render
 
-  # This allows you to change x,y by using @board[x, y] = val
+  # This trick allows you to change x,y by using @array[x, y] = val
   def []=(x, y, val)
-    @board[y][x] = val
+    @array[y][x] = val
   end
 
 end # class Board
@@ -129,18 +138,19 @@ def coords_test
     end
   end
 end
-pp coords_test
+# pp coords_test
 
 board = Board.new
 
 c = Piece.new('c', -1, 4, 5, board)
 m = Piece.new('m', 1, 3, 0, board)
 m = King.new(m, board)
+pp board.grid
 board.render
 c.r
 m.dl
 board.render
-
+# pp board.grid
 
 #
   # r = []
